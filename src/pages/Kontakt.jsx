da1bc65b0aafef1993285b2a04cdae80de9d4930
tarifalhasan <tarifalhasan999@gmail.com>
2023-03-24 19:49:16 +0600

    done project

--- src/pages/Kontakt.jsx
+++ src/pages/Kontakt.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import Layout from '../components/Layout';
 const Kontakt = () => {
   const animationVariants = {
     hover: {
@@ -74,96 +75,98 @@ const Kontakt = () => {
     duration: 0.6,
   };
   return (
-    <section className="container space-y-14 lg:py-16">
-      <div className=" w-[60%] mx-auto">
-        <motion.div
-          whileHover="hover"
-          initial="exit"
-          animate="enter"
-          exit="exit"
-          transition={transition}
-          className="text-4xl  space-y-3 texter lg:text-8xl font-neueMedium"
-        >
+    <Layout>
+      <section className="container space-y-14 lg:py-16">
+        <div className=" w-[60%] mx-auto">
           <motion.div
+            whileHover="hover"
+            initial="exit"
+            animate="enter"
+            exit="exit"
             transition={transition}
-            className=""
-            variants={animationVariants}
+            className="text-4xl  space-y-3 texter lg:text-8xl font-neueMedium"
           >
-            <h2 className="text-center pr-7">WER</h2>
-          </motion.div>
-
-          <motion.h2
-            className="ml-[50%] font-Fontspring text-center"
-            transition={transition}
-            variants={animationVariants2}
-          >
-            DAHINTER
-          </motion.h2>
-
-          <>
             <motion.div
               transition={transition}
-              variants={animationVariants3}
-              className="flex justify-center"
+              className=""
+              variants={animationVariants}
             >
-              <span>STECKT </span>
+              <h2 className="text-center pr-7">WER</h2>
             </motion.div>
-          </>
-        </motion.div>
-      </div>
-      <div>
-        <img
-          className="max-h-[550px] w-full object-cover"
-          src="/Rahel_Quadratisch.png"
-          alt="Rahel_Quadratisch"
-        />
-      </div>
-      <div className=" space-y-5">
-        <motion.div
-          whileHover="hover"
-          initial="exit"
-          animate="enter"
-          exit="exit"
-          transition={transition}
-          className="text-xl lg:w-2/3 mx-auto px-16  space-y-3 texter lg:text-5xl font-neueMedium"
-        >
-          <motion.div
-            transition={transition}
-            className=""
-            variants={animationVariants4}
-          >
-            <h2 className="text-center w-full pr-7">HIER STEHT</h2>
-          </motion.div>
 
-          <motion.div transition={transition} variants={animationVariants5}>
-            <h2 className="ml-[20%] w-full font-Fontspring text-center">
-              EIN TITEL
-            </h2>
-          </motion.div>
-        </motion.div>
+            <motion.h2
+              className="ml-[50%] font-Fontspring text-center"
+              transition={transition}
+              variants={animationVariants2}
+            >
+              DAHINTER
+            </motion.h2>
 
-        <div className="text-center pb-11 lg:pb-0 text-2xl px-4 lg:px-10 font-neueLight">
-          <p>
-            Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
-            Erklärung zum Design. Hier steht eine kurze Erklärung zum Design.
-            Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
-            Erklärung zum Design. Hier steht eine kurze Erklärung zum Design.
-            Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
-            Erklärung zum Design. Hier steht eine kurze Erklärung zum Design.
-          </p>
-          <p>
-            Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
-            Erklärung zum Design. Hier steht eine kurze Erklärung zum Design.
-            Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
-            Erklärung zum Design. Hier steht eine kurze Erklärung zum Design.
-            Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
-            Erklärung zum Design. Hier steht eine kurze Erklärung zum Design.
-            Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
-            Erklärung zum Design.
-          </p>
+            <>
+              <motion.div
+                transition={transition}
+                variants={animationVariants3}
+                className="flex justify-center"
+              >
+                <span>STECKT </span>
+              </motion.div>
+            </>
+          </motion.div>
         </div>
-      </div>
-    </section>
+        <div>
+          <img
+            className="max-h-[550px] w-full object-cover"
+            src="/Rahel_Quadratisch.png"
+            alt="Rahel_Quadratisch"
+          />
+        </div>
+        <div className=" space-y-5">
+          <motion.div
+            whileHover="hover"
+            initial="exit"
+            animate="enter"
+            exit="exit"
+            transition={transition}
+            className="text-xl lg:w-2/3 mx-auto px-16  space-y-3 texter lg:text-5xl font-neueMedium"
+          >
+            <motion.div
+              transition={transition}
+              className=""
+              variants={animationVariants4}
+            >
+              <h2 className="text-center w-full pr-7">HIER STEHT</h2>
+            </motion.div>
+
+            <motion.div transition={transition} variants={animationVariants5}>
+              <h2 className="ml-[20%] w-full font-Fontspring text-center">
+                EIN TITEL
+              </h2>
+            </motion.div>
+          </motion.div>
+
+          <div className="text-center pb-11 lg:pb-0 text-2xl px-4 lg:px-10 font-neueLight">
+            <p>
+              Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
+              Erklärung zum Design. Hier steht eine kurze Erklärung zum Design.
+              Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
+              Erklärung zum Design. Hier steht eine kurze Erklärung zum Design.
+              Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
+              Erklärung zum Design. Hier steht eine kurze Erklärung zum Design.
+            </p>
+            <p>
+              Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
+              Erklärung zum Design. Hier steht eine kurze Erklärung zum Design.
+              Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
+              Erklärung zum Design. Hier steht eine kurze Erklärung zum Design.
+              Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
+              Erklärung zum Design. Hier steht eine kurze Erklärung zum Design.
+              Hier steht eine kurze Erklärung zum Design. Hier steht eine kurze
+              Erklärung zum Design.
+            </p>
+          </div>
+        </div>
+      </section>
+    </Layout>
   );
 };
 
